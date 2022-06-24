8de6fb7463e9f65958a084bfdf69ef8733280831
Gregory Potemkin <57456890+NmadeleiDev@users.noreply.github.com>
2022-06-24 15:15:22 +0300

    Update builder.py

--- builder.py
+++ builder.py
@@ -12,26 +12,18 @@ nginx_conf_template = 'server {{ \n\
 
 # parse args
 parser = argparse.ArgumentParser()
-parser.add_argument('--domain', help='Domain to create')
-parser.add_argument('--container-port', help='Port of local container')
+parser.add_argument('--domain', help='Domain to create', required=True)
+parser.add_argument('--container-port', help='Port of local container', required=True)
 args = parser.parse_args()
 
 domain = args.domain
 cont_port = args.container_port
-
-if (domain is None):
-    print("Specify fucking domain!! (by option --domain=[your_domain])")
-    exit(0)
-if (cont_port is None):
-    print("Specify fucking container port!! (by option --container-port=[your_port])")
-    exit(0)
 
 # create conf file
 conf_files_dir = '/etc/nginx/sites-available'
 conf_file = open(os.path.join(conf_files_dir, domain), "x")
 
 conf_file.write(nginx_conf_template.format(domain, cont_port))
-
 
 #create sites-enabled link
 create_link_command = 'ln -s /etc/nginx/sites-available/{} /etc/nginx/sites-enabled/{}'
